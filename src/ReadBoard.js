b208ca6b762489fbc016bde20edcea41fdd1412d
ìoxjadex <oharin765@gmail.com>
2023-09-21 08:24:37 +0900

    FEAT :: 삭제, 수정

--- src/ReadBoard.js
+++ src/ReadBoard.js
@@ -7,6 +7,8 @@ function ReadBoard() {
       <div id="mini-title">DevCoop 공지사항</div>
       <div className="board-box">
         <div className="main-title">테스트용 데브쿠프 공지사항</div>
+        <div id="edit">수정</div>
+        <div id="remove">삭제</div>
         <div className="mini-explanation">
           간단한 공지사항에 대한 설명이 들어갈 자리입니다. 간단한 공지사항에
           대한 설명이 들어갈 자리입니다. 간단한 공지사항에 대한 설명이 들어갈
