1b133ee6041689952e73d02eb28fd47bc75e3891
AnSiHyeon <ansh0209kr@gmail.com>
2023-09-24 22:03:27 +0900

    FEAT: : 회원가입에서 로그인으로 가는 링크 추가

--- src/Login.js
+++ src/Login.js
@@ -14,7 +14,7 @@ function Login() {
         <input type="password" name="userPassword" placeholder="비밀번호" /><br /><br />
         <input type="submit" id="login" value="로그인" />
       </form>
-      <text>만약 계정이 없다면<Link to="/Sign">회원가입하기</Link>
+      <text>만약 계정이 없다면?<Link to="/Sign">회원가입하기</Link>
 </text>
     </div>
   );
--- src/Sign.js
+++ src/Sign.js
@@ -1,5 +1,6 @@
 import './App.css';
 import chick from "./DevCoopL.svg"
+import { Link } from "react-router-dom";
 
 function Sign() {
   return (
@@ -10,8 +11,9 @@ function Sign() {
         <input type="text" name="userName" placeholder="이메일" /><br />
         <input type="password" name="userPassword" placeholder="비밀번호" /><br />
         <input type="password check" name="userPasswordcheck" placeholder="비밀번호 확인" /><br /><br />
-        <input type="submit" id="login" value="회원가입" />
+        <input type="submit" id="login" value="회원가입" /><br></br>
       </form>
+      <text>이미 계정이 있다면?<Link to="/Login">로그인</Link></text>
     </div>
   );
 }
